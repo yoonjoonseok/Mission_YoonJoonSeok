44d772e5cb6968cd548cf185ff6df3f432814c95
yoonjoonseok <yjsygm@naver.com>
2023-05-07 22:04:19 +0900

    (기능 추가) 상단바에 아직 읽지 않은 알림이 존재하는지를 인디케이터로 표시

--- src/main/java/com/ll/gramgram/boundedContext/notification/repository/NotificationRepository.java
+++ src/main/java/com/ll/gramgram/boundedContext/notification/repository/NotificationRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     List<Notification> findByToInstaMember(InstaMember toInstaMember);
+
+    int countByToInstaMemberAndReadDateIsNull(InstaMember instaMember);
 }
