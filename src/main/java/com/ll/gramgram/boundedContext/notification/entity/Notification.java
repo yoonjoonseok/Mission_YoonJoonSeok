e788ecb36efb1fde6b5318adb503ecdd98dea122
yoonjoonseok <yjsygm@naver.com>
2023-05-07 22:28:02 +0900

    (기능 추가 & 리팩토링) 1시간 이내 알림 불이모티콘 표시 & html 코드 중복 제거

--- src/main/java/com/ll/gramgram/boundedContext/notification/entity/Notification.java
+++ src/main/java/com/ll/gramgram/boundedContext/notification/entity/Notification.java
@@ -44,6 +44,11 @@ public class Notification extends BaseEntity {
         readDate = LocalDateTime.now();
     }
 
+    public boolean isHot() {
+        // 만들어진지 60분이 안되었다면 hot 으로 설정
+        return getCreateDate().isAfter(LocalDateTime.now().minusMinutes(60));
+    }
+
     public String getModifyDateDisplay() {
         LocalDateTime modifyDate = getModifyDate();
         LocalDateTime now = LocalDateTime.now();
